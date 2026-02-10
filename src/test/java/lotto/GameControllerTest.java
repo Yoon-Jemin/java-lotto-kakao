1f1cdfd077dbfc0a0ee1e1831f60e313b58b1b74
sapiensnoah <noah.so@kakaocorp.com>
2026-02-10 11:39:05 +0900

    feat: 게임 컨트롤러 성공케이스

--- src/test/java/lotto/GameControllerTest.java
+++ src/test/java/lotto/GameControllerTest.java
@@ -36,4 +36,31 @@ public class GameControllerTest {
         assertThat(controller.getUser()).isNotNull();
         assertThat(controller.getWinningLotto()).isNotNull();
     }
+
+    @Test
+    @DisplayName()
+    public void success_1() {
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(7, 8, 9, 10, 11, 12));
+        Lotto lotto3 = new Lotto(List.of(13, 14, 15, 16, 17, 18));
+        List<Lotto> lottos = List.of(lotto1, lotto2, lotto3);
+
+        Lotto winningLottoNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        User user = new User(3000, 3, lottos);
+        WinningLotto winningLotto = new WinningLotto(winningLottoNumber, 7);
+        Random numberGenerator = new RandomNumberGenerator();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        GameController controller = new GameController(
+                user,
+                winningLotto,
+                numberGenerator,
+                inputView,
+                outputView
+        );
+
+        controller.play();
+    }
 }
