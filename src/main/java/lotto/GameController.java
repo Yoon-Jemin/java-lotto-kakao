da2fadeeb486ed89b2bbdf49456c6b2b1e2594c0
sapiensnoah <noah.so@kakaocorp.com>
2026-02-10 14:42:33 +0900

    feat: 로또 결과 계산기

--- src/main/java/lotto/GameController.java
+++ src/main/java/lotto/GameController.java
@@ -6,19 +6,17 @@ import java.util.List;
 
 public class GameController {
 
-    private final User user;
+    private User user;
     private WinningLotto winningLotto;
     private final Random randomNumberGenerator;
     private final InputView inputView;
     private final OutputView outputView;
 
     public GameController(
-            User user,
             Random randomNumberGenerator,
             InputView inputView,
             OutputView outputView
     ) {
-        this.user = user;
         this.randomNumberGenerator = randomNumberGenerator;
         this.inputView = inputView;
         this.outputView = outputView;
@@ -33,34 +31,54 @@ public class GameController {
     }
 
     public GameResult play() {
-        outputView.printMessage("구입금액을 입력해 주세요.");
-        int price = Integer.parseInt(inputView.input());
-        int lottoCount = price / 1000;
-        outputView.printMessage(lottoCount + "개를 구매했습니다.");
+        makeUserInfo();
+        makeWinningLotto();
+        LottoResultCalculator calculator = new LottoResultCalculator(user, winningLotto);
+        return calculator.calculate();
+//        return new GameResult(GameStatus.FOUR_CORRECT, 10000, 1);
+    }
 
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < lottoCount; i++) {
-            lottos.add(new Lotto(randomNumberGenerator.generate()));
-        }
-
-        User user = new User(price, lottoCount, lottos);
-        for (Lotto lotto : user.getLottos()) {
-            outputView.printLog(lotto.getNumbers());
-        }
-
-        outputView.printMessage("지난 주 당첨 번호를 입력해 주세요.");
-        String winningLottoStr = inputView.input();
-        String[] winningLottoArray = winningLottoStr.split(",");
+    private void makeWinningLotto() {
+        String[] winningLottoArray = makeWinningLottoNumbers();
 
         List<Integer> winningLottoList = Arrays.stream(winningLottoArray)
                 .map(Integer::parseInt)
                 .toList();
 
-        outputView.printMessage("보너스 볼을 입력해 주세요.");
-        int bonusNumber = Integer.parseInt(inputView.input());
-
+        int bonusNumber = makeBonusNumber();
         winningLotto = new WinningLotto(new Lotto(winningLottoList), bonusNumber);
-
-        return new GameResult(GameStatus.FOUR_CORRECT, 10000, 1);
     }
+
+    private int makeBonusNumber() {
+        outputView.printMessage("보너스 볼을 입력해 주세요.");
+        return Integer.parseInt(inputView.input());
+    }
+
+    private String[] makeWinningLottoNumbers() {
+        outputView.printMessage("지난 주 당첨 번호를 입력해 주세요.");
+        String winningLottoStr = inputView.input();
+        return winningLottoStr.split(",");
+    }
+
+    private void makeUserInfo() {
+        outputView.printMessage("구입금액을 입력해 주세요.");
+        int price = Integer.parseInt(inputView.input());
+        int lottoCount = price / 1000;
+        outputView.printMessage(lottoCount + "개를 구매했습니다.");
+
+        makeUserLottoInfo(lottoCount, price);
+    }
+
+    private void makeUserLottoInfo(int lottoCount, int price) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto(randomNumberGenerator.generate()));
+        }
+
+        this.user = new User(price, lottoCount, lottos);
+        for (Lotto lotto : user.getLottos()) {
+            outputView.printLog(lotto.getNumbers());
+        }
+    }
+
 }
--- src/main/java/lotto/GameResult.java
+++ src/main/java/lotto/GameResult.java
@@ -1,14 +1,37 @@
 package lotto;
 
+import java.util.List;
+
+import static lotto.GameStatus.*;
+
 public class GameResult {
 
-    private GameStatus gameStatus;
+    private List<GameStatus> statuses;
     private int profit;
     private double profitRate;
 
-    public GameResult(GameStatus gameStatus, int profit, double profitRate) {
-        this.gameStatus = gameStatus;
+    public GameResult(int profit, double profitRate) {
+        this.statuses = List.of(
+                FAIL,
+                THREE_CORRECT,
+                FOUR_CORRECT,
+                FIVE_CORRECT,
+                FIVE_CORRECT_BONUS, 
+                SIX_CORRECT
+        );
         this.profit = profit;
         this.profitRate = profitRate;
+    }
+
+    public List<GameStatus> getStatuses() {
+        return statuses;
+    }
+
+    public int getProfit() {
+        return profit;
+    }
+
+    public double getProfitRate() {
+        return profitRate;
     }
 }
