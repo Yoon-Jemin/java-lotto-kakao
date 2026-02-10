860aef8feb21b4b29c4eccb6b632e88d86ed5606
sapiensnoah <noah.so@kakaocorp.com>
2026-02-10 10:44:24 +0900

    feat: 당첨번호 + 보너스 번호

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,38 @@
+package lotto;
+
+public class WinningLotto {
+
+    private final Lotto lotto;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto lotto, int bonusNumber) {
+        validate(lotto, bonusNumber);
+        this.lotto = lotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(Lotto lotto, int bonusNumber) {
+        validateRange(bonusNumber);
+        validateDuplicate(lotto, bonusNumber);
+    }
+
+    private void validateRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("1 ~ 45 범위를 벗어나는 숫자가 입력되었습니다.");
+        }
+    }
+
+    private void validateDuplicate(Lotto lotto, int bonusNumber) {
+        if (lotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("당첨번호와 중복된 숫자를 보너스 번호로 등록할 수 없습니다.");
+        }
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
---
+++ src/test/java/lotto/WinningLottoTest.java
@@ -0,0 +1,50 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class WinningLottoTest {
+
+    @Test
+    @DisplayName("성공케이스")
+    void success() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+
+        assertThat(winningLotto.getLotto().getNumbers()).containsExactly(1,2,3,4,5,6);
+        assertThat(winningLotto.getBonusNumber()).isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 범위를 벗어나는 경우")
+    void fail_bonusNumberRange() {
+
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+            int bonusNumber = 46;
+            WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1 ~ 45 범위를 벗어나는 숫자가 입력되었습니다.");
+
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 당첨 번호와 중복되는 경우")
+    void fail_bonusNumberDuplicate() {
+
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+            int bonusNumber = 1;
+            WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("당첨번호와 중복된 숫자를 보너스 번호로 등록할 수 없습니다.");
+
+    }
+
+}
