7857dcac9972d9ef2d0d1188cb4f0c088f30293c
yukon.yoon <yukon.yoon@kakaocorp.com>
2026-02-10 10:26:39 +0900

    feat: 로또 일급 객체 생성 및 테스트

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,15 +1,41 @@
 package lotto;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
 
     private List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validate(numbers);
         this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        for (int number : numbers) {
+            ValidateRange(number);
+        }
+        validateDuplicate(numbers);
+        validateNumberCount(numbers);
+    }
+
+    private void validateNumberCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("입력된 숫자가 6개가 아닙니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("중복된 숫자가 입력되었습니다.");
+        }
+    }
+
+    private void ValidateRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("1 ~ 45 범위를 벗어나는 숫자가 입력되었습니다.");
+        }
     }
 
     public List<Integer> getNumbers() {
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -16,4 +16,28 @@ public class LottoTest {
         Assertions.assertThat(lotto.getNumbers()).hasSize(6);
         Assertions.assertThat(lotto.getNumbers()).containsExactly(1,2,3,4,5,6);
     }
+
+    @Test
+    @DisplayName("1 ~ 45 범위를 벗어나는 숫자인 경우")
+    void lottoFailTest1() {
+        Assertions.assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto(List.of(1,2,3,4,5,99));
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("1 ~ 45 범위를 벗어나는 숫자가 입력되었습니다.");
+    }
+
+    @Test
+    @DisplayName("중복된 숫자를 입력하는 경우")
+    void lottoFailTest2() {
+        Assertions.assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto(List.of(1,2,3,4,5,5));
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("중복된 숫자가 입력되었습니다.");
+    }
+
+    @Test
+    @DisplayName("숫자가 6개가 아닌 경우")
+    void lottoFailTest3() {
+        Assertions.assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto(List.of(1,2,3,4,5,6,7));
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("입력된 숫자가 6개가 아닙니다.");
+    }
 }
